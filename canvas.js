fce621e6f27b90d5b51fd0944b6aa158212090df
zensations <pm@zensations.at>
2011-05-20 15:24:47 +0200

    Fixes, ready for Zweiraum app.

--- canvas.js
+++ canvas.js
@@ -1,12 +1,14 @@
 (function ($) {
   Drupal.behaviors.social_facebook = {
     attach: function(context, settings) {
+      $('body').append('<div id="fb-root">&nbsp;</div>');
       FB.init({
-         appId  : settings.canvas.appid,
-         status : true, // check login status
-         cookie : true, // enable cookies to allow the server to access the session
-         xfbml  : true  // parse XFBML
-       });
+        appId  : settings.canvas.appid,
+        status : true, // check login status
+        cookie : true, // enable cookies to allow the server to access the session
+        xfbml  : true  // parse XFBML
+      });
+      FB.Canvas.setAutoResize(); 
     }
   };
 })(jQuery);
---
+++ canvas_like.tpl.php
@@ -0,0 +1,1 @@
+<h2>Please like this page to proceed.</h2>
